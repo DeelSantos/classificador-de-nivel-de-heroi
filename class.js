78155918b8cf92d87de2204ce977cda51d5b26b5
Edelson Santos <edelsonjr.4@hotmail.com>
2024-08-28 21:15:18 -0300

    Update class.js

--- class.js
+++ class.js
@@ -2,25 +2,25 @@ const prompt = require('prompt-sync')();
 
 
 let nomeHeroi = prompt('Digite o nome do Herói: ');
-let xpHeroi = prompt('Digite o nível do Herói: ');
+let xpHeroi = prompt('Digite o xp do Herói: ');
 
 if(xpHeroi < 1000) {
     console.log(`O Herói ${nomeHeroi} está no nível de ${xpHeroi} e está no ranking Ferro`);
     
 } else if (xpHeroi > 1000 && xpHeroi <= 2000) {
-    console.log(`O Herói ${nomeHeroi} está no nível de ${xpHeroi} e está no ranking Bronze`);
+    console.log(`O Herói ${nomeHeroi} está com ${xpHeroi}XP e está no ranking Bronze`);
 } else if (xpHeroi > 2000 && xpHeroi <= 5000) {
-    console.log(`O Herói ${nomeHeroi} está no nível de ${xpHeroi} e está no ranking Prata`);
+    console.log(`O Herói ${nomeHeroi} está com ${xpHeroi}XP e está no ranking Prata`);
 } else if (xpHeroi > 5000 && xpHeroi <= 7000) {
-    console.log(`O Herói ${nomeHeroi} está no nível de ${xpHeroi} e está no ranking Ouro`);
+    console.log(`O Herói ${nomeHeroi} está com ${xpHeroi}XP e está no ranking Ouro`);
 } else if (xpHeroi > 7000 && xpHeroi <= 8000) {
-    console.log(`O Herói ${nomeHeroi} está no nível de ${xpHeroi} e está no ranking Platina`);
+    console.log(`O Herói ${nomeHeroi} está com ${xpHeroi}XP e está no ranking Platina`);
 } else if (xpHeroi > 8000 && xpHeroi <= 9000) {
-    console.log(`O Herói ${nomeHeroi} está no nível de ${xpHeroi} e está no ranking Ascendente`);
+    console.log(`O Herói ${nomeHeroi} está com ${xpHeroi}XP e está no ranking Ascendente`);
 } else if (xpHeroi > 9000 && xpHeroi <= 10000) {
-    console.log(`O Herói ${nomeHeroi} está no nível de ${xpHeroi} e está no ranking Imortal`);
+    console.log(`O Herói ${nomeHeroi} está com ${xpHeroi}XP e está no ranking Imortal`);
 } else{
-    console.log(`O Herói ${nomeHeroi} está no nível de ${xpHeroi} e está no ranking Radiante`);
+    console.log(`O Herói ${nomeHeroi} está com ${xpHeroi}XP e está no ranking Radiante`);
 }
 
 
